2066537544195d78d004ff1024509e3ebc22ac28
xiuxiu <1831894878@qq.com>
2024-06-04 13:52:10 +0800

    finish linker lab :)

--- ics-ld-public/src/relocation.cc
+++ ics-ld-public/src/relocation.cc
@@ -1,13 +1,30 @@
 #include "relocation.h"
+#include <iostream>
 
 #include <sys/mman.h>
 
 void handleRela(std::vector<ObjectFile> &allObject, ObjectFile &mergedObject, bool isPIE)
 {
-    /* When there is more than 1 objects, 
+    /* When there is more than 1 objects,
      * you need to adjust the offset of each RelocEntry
      */
     /* Your code here */
+    //it's too hard!!
+    uint64_t secSize[5] = {0, 0, 0, 0, 0};
+    for (unsigned index = 0; index < 5; index++) {
+        for (auto &obj : allObject) {
+            for (auto &re : obj.relocTable) {
+                //std::cout << "-----------------------------------" << std::endl;
+                //std::cout << re.sym->type << std::endl;
+                if (re.sym->type == (int) index) {
+                    re.offset += secSize[index];
+                    //std::cout << re.offset << std::endl;
+                    //std::cout << "-----------------------------------" << std::endl;
+                }
+            }
+            secSize[index] += obj.sectionsByIdx[index]->size;
+        }
+    }
 
     /* in PIE executables, user code starts at 0xe9 by .text section */
     /* in non-PIE executables, user code starts at 0xe6 by .text section */
@@ -16,4 +33,28 @@ void handleRela(std::vector<ObjectFile> &allObject, ObjectFile &mergedObject, bo
     uint64_t textAddr = mergedObject.sections[".text"].addr + userCodeStart;
 
     /* Your code here */
+    for (auto &obj : allObject) {
+        for (auto &re : obj.relocTable) {
+            //std::cout << re.type << std::endl;
+            if (re.type == 2) {
+                uint64_t refptr = (uint64_t)mergedObject.baseAddr + textOff + re.offset;
+                int64_t valueToFill = re.sym->value - (textAddr + re.offset) + re.addend;
+                *reinterpret_cast<int *>(refptr) = valueToFill;
+                //std::cout << refptr << std::endl;
+            }
+            if (re.type == 4) {
+                uint64_t refptr = (uint64_t)mergedObject.baseAddr + textOff + re.offset;
+                int64_t valueToFill = re.sym->value - (textAddr + re.offset) + re.addend;
+                *reinterpret_cast<int *>(refptr) = valueToFill;
+                //std::cout << refptr << std::endl;
+            }
+            if (re.type == 10) {
+                uint64_t refptr = (uint64_t)mergedObject.baseAddr + textOff + re.offset;
+                int64_t valueToFill = re.sym->value + re.addend;
+                *reinterpret_cast<int *>(refptr) = valueToFill;
+                //std::cout << refptr << std::endl;
+            }
+        }
+    }
+
 }
--- ics-ld-public/src/resolve.cc
+++ ics-ld-public/src/resolve.cc
@@ -31,5 +31,40 @@ int callResolveSymbols(std::vector<ObjectFile> &allObjects)
     // if found multiple definition, set the errSymName to problematic symbol name and return MULTIDEF;
     // if no definition is found, set the errSymName to problematic symbol name and return NODEF;
 
+    for (auto &obj : allObjects) {
+        //printSymbolTable(obj);
+        for (auto &re : obj.relocTable) {
+            int flag = -1; //no definition
+            Symbol *prioritySym = re.sym;
+            for (auto &obj1 : allObjects) {
+                for (auto &sym : obj1.symbolTable) {
+
+                    if (sym.name == re.name && sym.bind == STB_GLOBAL && sym.index != SHN_UNDEF && sym.index != SHN_COMMON) {
+                        //strong symbols
+                        if (flag == 1) {
+                            errSymName += re.name;
+                            return MULTI_DEF;
+                        }
+                        flag = 1;
+                        prioritySym = &sym;
+                    } else if (sym.name == re.name && sym.bind == STB_GLOBAL && sym.index == SHN_COMMON) {
+                        //weak symbols
+                        if (flag != 1) {
+                            flag = 0;
+                            prioritySym = &sym;
+                        }
+                    }
+                }
+            }
+
+            if (flag == -1) {
+                errSymName += re.name;
+                return NO_DEF;
+            } else {
+                re.sym = prioritySym;
+            }
+        }
+    }
+
     return FOUND_ALL_DEF;
 }
